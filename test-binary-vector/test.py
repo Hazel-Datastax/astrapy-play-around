42a19ec7cea80e3ba8881a7eba0cfcef023a91ca
Hazel <hazel.he@datastax.com>
2024-11-13 14:23:00 -0800

    Update test

--- test-binary-vector/test.py
+++ test-binary-vector/test.py
@@ -27,21 +27,21 @@ db = DataAPIClient(
 db.get_database_admin().create_keyspace("default_keyspace", update_db_keyspace=True)
 
 
-def vector_insert_one(count: int, dimension: int):
-    vector = [{"$vector": np.random.rand(dimension).tolist()} for _ in range(count)]
+def vector_insert_one(num_of_doc: int, dimension: int):
+    vector = [{"$vector": np.random.rand(dimension).tolist()} for _ in range(num_of_doc)]
     collection_name = inspect.currentframe().f_code.co_name
     # create a collection and write to it
     collection = db.create_collection(collection_name, dimension=dimension)
     start = time.time()
-    for i in range(count):
+    for i in range(num_of_doc):
         collection.insert_one(vector[i])
     end = time.time()
     db.drop_collection(collection_name)
     return end - start
 
 
-def vector_insert_many(count: int, dimension: int):
-    vector = [{"$vector": np.random.rand(dimension).tolist()} for _ in range(count)]
+def vector_insert_many(num_of_doc: int, dimension: int):
+    vector = [{"$vector": np.random.rand(dimension).tolist()} for _ in range(num_of_doc)]
     collection_name = inspect.currentframe().f_code.co_name
     # create a collection and write to it
     collection = db.create_collection(collection_name, dimension=dimension)
@@ -52,20 +52,20 @@ def vector_insert_many(count: int, dimension: int):
     return end - start
 
 
-def binary_vector_insert_one(count: int, dimension: int):
-    binary_vector = [{"$vector": DataAPIVector(np.random.rand(dimension).tolist())} for _ in range(count)]
+def binary_vector_insert_one(num_of_doc: int, dimension: int):
+    binary_vector = [{"$vector": DataAPIVector(np.random.rand(dimension).tolist())} for _ in range(num_of_doc)]
     collection_name = inspect.currentframe().f_code.co_name
     collection = db.create_collection(collection_name, dimension=dimension)
     start = time.time()
-    for i in range(count):
+    for i in range(num_of_doc):
         collection.insert_one(binary_vector[i])
     end = time.time()
     db.drop_collection(collection_name)
     return end - start
 
 
-def binary_vector_insert_many(count: int, dimension: int):
-    binary_vector = [{"$vector": DataAPIVector(np.random.rand(dimension).tolist())} for _ in range(count)]
+def binary_vector_insert_many(num_of_doc: int, dimension: int):
+    binary_vector = [{"$vector": DataAPIVector(np.random.rand(dimension).tolist())} for _ in range(num_of_doc)]
     collection_name = inspect.currentframe().f_code.co_name
     collection = db.create_collection(collection_name, dimension=dimension)
     start = time.time()
@@ -76,15 +76,20 @@ def binary_vector_insert_many(count: int, dimension: int):
 
 
 def main():
-    results = []
-    for func in [vector_insert_one, vector_insert_many, binary_vector_insert_one, binary_vector_insert_many]:
-        result = func(1000, 1000)
-        results.append((func.__name__, result))
+    num_of_docs = [1000, 10000, 100000, 1000000]
+    vector_dimensions = [256, 512, 1024, 4096]
+    for num_of_doc in num_of_docs:
+        for dimension in vector_dimensions:
+            print(f"=============== Number of docs: {num_of_doc}, Dimension: {dimension} ===============")
+            results = []
+            for func in [vector_insert_one, vector_insert_many, binary_vector_insert_one, binary_vector_insert_many]:
+                result = func(num_of_doc, dimension)
+                results.append((func.__name__, result))
 
-    results.sort(key=lambda x: x[1])
-    slowest = results[-1][1]
-    for name, duration in results:
-        print(f"{name}: \t{duration:.12f} seconds \t {duration / slowest:.2f}x")
+            results.sort(key=lambda x: x[1])
+            slowest = results[-1][1]
+            for name, duration in results:
+                print(f"{name}: \t{duration:.12f} seconds \t {duration / slowest:.2f}x")
 
 
 if __name__ == "__main__":
